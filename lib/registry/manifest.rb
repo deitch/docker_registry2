736e83611549ae7e10c9f124911087acc092e2ff
Avi Deitcher <avi@deitcher.net>
2022-12-23 15:02:44 +0200

    remove deprecated action
    Signed-off-by: Avi Deitcher <avi@deitcher.net>

--- lib/registry/manifest.rb
+++ lib/registry/manifest.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 module DockerRegistry2
+  # Manifest class represents a manfiest or index in an OCI registry
   class Manifest < Hash
     attr_accessor :body, :headers
   end
