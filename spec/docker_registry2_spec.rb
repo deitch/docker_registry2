a178b357e6952853795759f10eb3bb4941049904
Yeikel <email@yeikel.com>
2023-04-27 01:23:04 +0000

    fix: use image manifests to extract the digest

--- spec/docker_registry2_spec.rb
+++ spec/docker_registry2_spec.rb
@@ -80,7 +80,7 @@ RSpec.describe DockerRegistry2 do
       end
 
       it { expect { my_ubuntu_multiarch_manifest }.not_to raise_error }
-      it { expect(archs).to match_array(%w[amd64 amd64]) }
+      it { expect(archs).to match_array(%w[amd64 arm64]) }
     end
 
     context 'Docker registry without path' do
@@ -98,6 +98,80 @@ RSpec.describe DockerRegistry2 do
 
       it 'The @path is included' do
         expect(registry.instance_variable_get(:@base_uri)).to eq('https://registry.myCompany.com:443/dockerproxy')
+      end
+    end
+
+    context 'Extracts the digest of an image' do
+      let(:uri) { 'http://localhost:5000' }
+      let(:registry) { DockerRegistry2::Registry.new(uri) }
+
+      it 'Digest is extracted from a manifest with single arch' do
+        VCR.use_cassette('manifest/ubuntu') do
+          expect(connected_object.digest('my-image', '2.0')).to eq('sha256:1815c82652c03bfd8644afda26fb184f2ed891d921b20a0703b46768f9755c57')
+        end
+      end
+      it 'Digest is extracted from a multiarch image' do
+        VCR.use_cassette('manifest/multiarch_ubuntu') do
+          expect(connected_object.digest('my-ubuntu', '17.04', 'amd64', 'linux')).to eq('sha256:213e05583a7cb8756a3f998e6dd65204ddb6b4c128e2175dcdf174cdf1877459')
+        end
+
+        VCR.use_cassette('manifest/multiarch_ubuntu') do
+          expect(connected_object.digest('my-ubuntu', '17.04', 'arm64', 'linux')).to eq('sha256:213e05583a7cb8756a3f998e6dd65204ddb6b4c128e2175dcdf174cdf1877459')
+        end
+      end
+
+      it 'Digest is extracted from a multiarch image with variant' do
+        VCR.use_cassette('manifest/multiarch_php_variant') do
+          expect(connected_object.digest('php', 'latest', 'arm', 'linux', 'v5')).to eq('sha256:1eb3215f71b6dcf1a1f9bec5fde07ae166ecf43de16e48ebdff3641ee54cac72')
+        end
+      end
+
+      manifests = [
+        {
+          'mediaType' => 'application/vnd.docker.distribution.manifest.v2+json',
+          'size' => 1357,
+          'digest' => 'sha256:213e05583a7cb8756a3f998e6dd65204ddb6b4c128e2175dcdf174cdf1877459',
+          'platform' => {
+            'architecture' => 'amd64',
+            'os' => 'linux'
+          }
+        },
+        {
+          'mediaType' => 'application/vnd.docker.distribution.manifest.v2+json',
+          'size' => 1357,
+          'digest' => 'sha256:213e05583a7cb8756a3f998e6dd65204ddb6b4c128e2175dcdf174cdf1877459',
+          'platform' => {
+            'architecture' => 'arm64',
+            'os' => 'linux'
+          }
+        }
+      ]
+
+      it 'When it a multiarch image and no arch/os are specified it returns the manifests' do
+        VCR.use_cassette('manifest/multiarch_ubuntu') do
+          expect(connected_object.digest('my-ubuntu', '17.04')).to eq(manifests)
+        end
+      end
+
+      it 'When it a multiarch image and only arch is given it returns the manifests' do
+        VCR.use_cassette('manifest/multiarch_ubuntu') do
+          expect(connected_object.digest('my-ubuntu', '17.04', 'arm64')).to eq(manifests)
+        end
+      end
+
+      it 'When it a multiarch image and only os is given it returns the manifests' do
+        VCR.use_cassette('manifest/multiarch_ubuntu') do
+          expect(connected_object.digest('my-ubuntu', '17.04', nil, 'linux')).to eq(manifests)
+        end
+      end
+
+      it 'Fails when there are no matches' do
+        VCR.use_cassette('manifest/multiarch_ubuntu') do
+          expect do
+            connected_object.digest('my-ubuntu',
+                                    '17.04', 'arm64', 'windows')
+          end.to raise_error(DockerRegistry2::NotFound, 'No matches found for the image=my-ubuntu tag=17.04 os=windows architecture=arm64')
+        end
       end
     end
   end
