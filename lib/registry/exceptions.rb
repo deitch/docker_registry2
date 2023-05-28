deitch/docker_registry2

commit ee6598b000639e4132460ddd72da6cd233205074

Fix rubocop autocorrect suggestions

lib/registry/exceptions.rb
@@ -1,32 +1,33 @@
+# frozen_string_literal: true
+
 module DockerRegistry2
   class Exception < RuntimeError
-    
-  end
-  
-  class RegistryAuthenticationException < Exception
   end
 
-  class RegistryAuthorizationException < Exception
+  class RegistryAuthenticationException < StandardError
   end
 
-  class RegistryUnknownException < Exception
+  class RegistryAuthorizationException < StandardError
   end
 
-  class RegistrySSLException < Exception
+  class RegistryUnknownException < StandardError
   end
 
-  class RegistryVersionException < Exception
-  end
-  
-  class ReauthenticatedException < Exception
-  end
-  
-  class UnknownRegistryException < Exception
+  class RegistrySSLException < StandardError
   end
 
-  class NotFound < Exception
+  class RegistryVersionException < StandardError
   end
 
-  class InvalidMethod < Exception
+  class ReauthenticatedException < StandardError
+  end
+
+  class UnknownRegistryException < StandardError
+  end
+
+  class NotFound < StandardError
+  end
+
+  class InvalidMethod < StandardError
   end
 end

Gemfile
@@ -1,3 +1,5 @@
-source "https://rubygems.org"
+# frozen_string_literal: true
+
+source 'https://rubygems.org'
 
 gemspec

alpine_manifest_test.rb
@@ -1,9 +1,10 @@
-require './lib/docker_registry2.rb'
+# frozen_string_literal: true
+
+require './lib/docker_registry2'
 
 reg = DockerRegistry2.connect
 man = reg.manifest 'library/alpine', 'latest'
-puts "MANIFEST"
+puts 'MANIFEST'
 puts man
-puts "HEADER"
+puts 'HEADER'
 puts man.headers
-

docker_registry2.gemspec
@@ -1,5 +1,6 @@
-# coding: utf-8
-lib = File.expand_path('../lib', __FILE__)
+# frozen_string_literal: true
+
+lib = File.expand_path('lib', __dir__)
 $LOAD_PATH.unshift(lib) unless $LOAD_PATH.include?(lib)
 require 'registry/version'
 
@@ -7,19 +8,21 @@ Gem::Specification.new do |spec|
   spec.name          = 'docker_registry2'
   spec.version       = DockerRegistry2::VERSION
   spec.authors       = [
-                         'Avi Deitcher https://github.com/deitch',
-                         'Jonathan Hurter https://github.com/johnsudaar',
-                         'Dmitry Fleytman https://github.com/dmitryfleytman',
-                         'Grey Baker https://github.com/greysteil'
-                       ]
+    'Avi Deitcher https://github.com/deitch',
+    'Jonathan Hurter https://github.com/johnsudaar',
+    'Dmitry Fleytman https://github.com/dmitryfleytman',
+    'Grey Baker https://github.com/greysteil'
+  ]
   spec.summary       = 'Docker v2 registry HTTP API client'
   spec.description   = 'Docker v2 registry HTTP API client with support for token authentication'
   spec.homepage      = 'https://github.com/deitch/docker_registry2'
   spec.license       = 'MIT'
 
-  spec.files         = %w{README.md} + Dir.glob("*.gemspec") + Dir.glob("{lib}/**/*", File::FNM_DOTMATCH).reject { |f| File.directory?(f) }
-  spec.executables   = spec.files.grep(/^bin\//) { |f| File.basename(f) }
-  spec.test_files    = spec.files.grep(/^(test|spec|features)\//)
+  spec.files         = %w[README.md] + Dir.glob('*.gemspec') + Dir.glob('{lib}/**/*', File::FNM_DOTMATCH).reject do |f|
+                                                                 File.directory?(f)
+                                                               end
+  spec.executables   = spec.files.grep(%r{^bin/}) { |f| File.basename(f) }
+  spec.test_files    = spec.files.grep(%r{^(test|spec|features)/})
   spec.require_paths = ['lib']
 
   spec.add_development_dependency 'bundler'

get-gem-name.rb
@@ -1,6 +1,7 @@
 #!ruby
+# frozen_string_literal: true
 
-require "rubygems"
+require 'rubygems'
 
-spec = Gem::Specification::load("docker_registry2.gemspec")
-puts spec.name.to_s+'-'+spec.version.to_s+'.gem'
+spec = Gem::Specification.load('docker_registry2.gemspec')
+puts "#{spec.name}-#{spec.version}.gem"

lib/docker_registry2.rb
@@ -1,13 +1,14 @@
-require File.dirname(__FILE__) + '/registry/version'
-require File.dirname(__FILE__) + '/registry/registry'
-require File.dirname(__FILE__) + '/registry/exceptions'
-require File.dirname(__FILE__) + '/registry/manifest'
-require File.dirname(__FILE__) + '/registry/blob'
+# frozen_string_literal: true
 
+require "#{File.dirname(__FILE__)}/registry/version"
+require "#{File.dirname(__FILE__)}/registry/registry"
+require "#{File.dirname(__FILE__)}/registry/exceptions"
+require "#{File.dirname(__FILE__)}/registry/manifest"
+require "#{File.dirname(__FILE__)}/registry/blob"
 
 module DockerRegistry2
-  def self.connect(uri="https://registry.hub.docker.com",opts={})
-    @reg = DockerRegistry2::Registry.new(uri,opts)
+  def self.connect(uri = 'https://registry.hub.docker.com', opts = {})
+    @reg = DockerRegistry2::Registry.new(uri, opts)
   end
 
   def self.search(query = '')
@@ -18,7 +19,7 @@ module DockerRegistry2
     @reg.tags(repository)
   end
 
-  def self.manifest(repository,tag)
-    @reg.manifest(repository,tag)
+  def self.manifest(repository, tag)
+    @reg.manifest(repository, tag)
   end
 end

lib/registry/blob.rb
@@ -1,7 +1,9 @@
+# frozen_string_literal: true
+
 module DockerRegistry2
   class Blob
     attr_reader :body, :headers
-    
+
     def initialize(headers, body)
       @headers = headers
       @body = body

lib/registry/manifest.rb
@@ -1,9 +1,7 @@
-module DockerRegistry2
+# frozen_string_literal: true
 
+module DockerRegistry2
   class Manifest < Hash
     attr_accessor :body, :headers
-    def initialize
-      super
-    end
   end
 end

commit a91d436c9ca8fdb36d6a26901c0894b4efcc8de0

restore exceptions inheriting from parent class
ee6598b accidentally changed the parent class of all exceptions from
DockerRegistry2::Exception to ::StandardError. This commit restores the
previous parent class and includes its module name so rubocop doesn't
complain and re-recommend StandardError.

lib/registry/exceptions.rb
@@ -4,30 +4,30 @@ module DockerRegistry2
   class Exception < RuntimeError
   end
 
-  class RegistryAuthenticationException < StandardError
+  class RegistryAuthenticationException < DockerRegistry2::Exception
   end
 
-  class RegistryAuthorizationException < StandardError
+  class RegistryAuthorizationException < DockerRegistry2::Exception
   end
 
-  class RegistryUnknownException < StandardError
+  class RegistryUnknownException < DockerRegistry2::Exception
   end
 
-  class RegistrySSLException < StandardError
+  class RegistrySSLException < DockerRegistry2::Exception
   end
 
-  class RegistryVersionException < StandardError
+  class RegistryVersionException < DockerRegistry2::Exception
   end
 
-  class ReauthenticatedException < StandardError
+  class ReauthenticatedException < DockerRegistry2::Exception
   end
 
-  class UnknownRegistryException < StandardError
+  class UnknownRegistryException < DockerRegistry2::Exception
   end
 
-  class NotFound < StandardError
+  class NotFound < DockerRegistry2::Exception
   end
 
-  class InvalidMethod < StandardError
+  class InvalidMethod < DockerRegistry2::Exception
   end
 end